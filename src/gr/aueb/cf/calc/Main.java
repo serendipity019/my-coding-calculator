54a890aa603634ddb9d3ae1f5a88a6139a5cc3bb
serendipity019 <navagio09@gmail.com>
2024-12-17 10:50:41 +0200

    Project created

---
+++ src/gr/aueb/cf/calc/Main.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.calc;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int sub(int num1, int num2){
+        return num1 - num2;
+    }
+}
